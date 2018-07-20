013f291c98ae5a7ed801eb7d3e0c5d6951f2f08a
= <nonguyen@ucsd.edu>
2018-07-20 01:40:14 -0700

    Generate general triagle window for msg

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ What I hope to get out of this project:
 File List:
 index.html -- The playground html file for hosting the javascript file.
 phrases -- List of magic 8 ball strings.  
+
+References:
+
+Found this page to learn about radial-gradients.
+    https://modernweb.com/css-spheres-2/
+
+Didn't use this, but interesting playground for learning html.
+    http://jsfiddle.net/GR4Kj/
+
+How to draw in an html canvas
+    https://www.kirupa.com/html5/drawing_triangles_on_the_canvas.htm
--- magic.js
+++ magic.js
@@ -6,10 +6,13 @@
 
 "use strict"
 
-const max_num_shakes = 12;
-const min_num_skakes = 3;
+const max_num_shakes = 9;
+const min_num_skakes = 5;
 const max_shake_distance = 150;
 const min_shake_distance = 30;
+const shake_speed = 100;
+const even_divisor = 2;
+const shake_direction = 0.5
 
 $(document).ready(function(evt) {
     var iii; // Counter for number of shakes.
@@ -23,7 +26,7 @@ $(document).ready(function(evt) {
 
         // Randomize start left or start right.
         var start = Math.random()
-        if (start < 0.5) {
+        if (start < shake_direction) {
             iii = 1; 
         } else {
             iii = 0;
@@ -41,16 +44,33 @@ $(document).ready(function(evt) {
             shake_distance = String(shake_distance) + "px";
 
             // Even shake, move left.
-            if (iii % 2 === 0) {
-                $(this).animate({ left: shake_distance}, 100); 
+            if (iii % even_divisor === 0) {
+                $(this).animate({ left: shake_distance}, shake_speed); 
             } 
             // Odd shake, move right.
             else {
                 // Make sure to shake in negative direction.
                 shake_distance = "-" + shake_distance; 
-                $(this).animate({ left: shake_distance}, 100); 
+                $(this).animate({ left: shake_distance}, shake_speed); 
             } 
         } 
-        $(this).animate({ left: '0px'}, 200); // Recenter ball.
+        $(this).animate({ left: '0px'}, shake_speed); // Recenter ball.
+
+        // Begin the reveal. Fade out the inner cirle and show the triangle.
     }); 
+
+    // Draw the item in the canvas.
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d"); 
+
+    context.beginPath();
+    context.moveTo(125, 0);
+    context.lineTo(25, 100);
+    context.lineTo(225, 100);
+    context.closePath(); 
+
+    context.fillStyle = "#FF0000";
+    context.fill();
+//    context.fillRect(0,0,150,75);
 });
+
